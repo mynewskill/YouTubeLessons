d94eb09594505bf74480c8abc4196731fb453704
Pavel Kuzmin <diandtv@gmail.com>
2021-06-07 04:17:20 +0300

    Add js logic for hamburger menu

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # YouTube Lessons
+
+`#`
--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello youtube");
+const btnHamburger = document.querySelector("#btnHamburger");
+btnHamburger.addEventListener("click", function () {
+  console.log("open hamburger");
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
